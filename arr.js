92ca4237e05f923c099053b2108f7ee284320973
Evgeniy Kvach <ypkvach@mts.ru>
2022-01-08 19:20:59 +0300

    arr

---
+++ arr.js
@@ -0,0 +1,13 @@
+// работа с массивом, получение частей массива
+// первый элемент массива
+const head = _.head(arr);
+// все кроме первого
+const tail = _.tail(arr);
+// последний
+const last = _.last(arr);
+// все, кроме последнего элемента
+const initial = _.initial(arr);
+const initialClassic = arr.slice(0, -1);
+
+// console.log(initial);
+// console.log(initialClassic);
